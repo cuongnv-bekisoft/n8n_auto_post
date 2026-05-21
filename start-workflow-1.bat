64609b390ab4bfdc32669607bb9b33165e029d40
cuongnv <cuongnv@bekisoft.com>
2026-05-21 15:30:51 +0700

    build workflow

---
+++ start-workflow-1.bat
@@ -0,0 +1,10 @@
+@echo off
+
+:: Chờ n8n khởi động xong (30 giây)
+timeout /t 30 /nobreak
+
+:: Gọi webhook kích hoạt workflow #1 luôn
+curl -s http://localhost:5678/webhook/start-workflow-1
+
+:: Giữ cửa sổ n8n mở
+n8n start
